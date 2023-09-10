ce5db386ebb7ee3b11db25adaca5bf6c87af862b
aleksandar <vasic.aleksandar.bg@gmail.com>
2023-09-10 21:34:53 +0200

    Create components and pages

---
+++ components/Car/Detail/Description.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="mt-5">
+    <p class="mb-10">
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempora omnis iste veniam nihil. Possimus unde id eius assumenda. Nemo neque similique laudantium voluptatum ratione soluta at odit quasi placeat atque.
+    </p>
+  </div>
+</template>
